08442fb0a71db61b8ac8caa9caa8e1c40b34b673
linden <linden.wang04@gmail.com>
2023-01-20 12:37:02 -0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
+import NavBar from './components/navbar';
 import Home from "./components/home";
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
 
