af2dbdacb46131133243a0700f9372c38bb0d57f
Brian Li <brianli4824@gmail.com>
2023-01-23 19:16:31 -0800

    about

--- country-news-finder/src/pages/About.js
+++ country-news-finder/src/pages/About.js
@@ -1,5 +1,10 @@
 const About = () => {
-  return <h1>About</h1>
+  return (
+    <div> 
+      <h1>About</h1>
+      <p>This project is in collaboration with UCD's Google Student Developer Club.</p>
+    </div>
+  )
 }
 
 export default About;
